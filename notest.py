2a2cab6c6dd8423d8c82e526c8c78af464376b61
Joshua "TechDev" Walker <jwtechdev@gmail.com>
2017-01-05 07:12:58 -0500

    initial python commit

---
+++ notest.py
@@ -0,0 +1,18 @@
+# creating functions
+# concept routine procedure that processes something and sometimes routines
+# something thats done over and over
+# steps to create a common task like making coffee or eggs
+
+# 1. shorter programs
+# 2. more robust code with less mistakes
+
+def make_coffee ():
+    print('fill with water')
+    print('insert k-cup')
+    print('hit button')
+    print('add cream')
+    print('add sugar')
+    return make_omelette
+
+josh_cup = make_omelette()
+emily_cup = make_omelette()
